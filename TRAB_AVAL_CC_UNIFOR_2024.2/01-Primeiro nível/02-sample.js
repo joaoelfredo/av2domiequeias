ecadbc78efef4fe46c57d5d11a048dd875334f8a
joaoelfredo <elfredojoao@gmail.com>
2024-11-12 19:13:43 -0300

    sample 02 corrigido!
    abordei nomes significativos, mais detalhes no próprio código :)

--- TRAB_AVAL_CC_UNIFOR_2024.2/01-Primeiro nível/02-sample.js
+++ TRAB_AVAL_CC_UNIFOR_2024.2/01-Primeiro nível/02-sample.js
@@ -1,12 +1,13 @@
-const a = true;
+//Tomei a liberdade e entender a variável "a" como login correto e a variável "u" como usuario, sendo as duas necessárias para conceder o acesso.
+const loginCorreto = true;
 
-function verAc(u) {
-    if (a && u.isAdmin) {
+function verificaAcesso(usuario) {
+    if (loginCorreto && usuario.isAdmin) {
         console.log("Acesso permitido ao administrador.");
     } else {
         console.log("Acesso negado.");
     }
 }
 
-const u = { nome: "João", admin: true };
-verAc(u);
+const usuario = { nome: "João", admin: true };
+verAc(usuario);
