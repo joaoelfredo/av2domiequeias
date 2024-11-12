7b259e275c878778bb6aff9c4f95aa4e862f9022
joaoelfredo <elfredojoao@gmail.com>
2024-11-12 19:03:42 -0300

    Correção da primeira amostra
    Nesse exercício tratei dos Números mágicos e nomes significativos

--- TRAB_AVAL_CC_UNIFOR_2024.2/01-Primeiro nível/01-sample.js
+++ TRAB_AVAL_CC_UNIFOR_2024.2/01-Primeiro nível/01-sample.js
@@ -1,11 +1,12 @@
-function h(a) {
-    const b = [];
+function encontraPares(a) {
+    const numerosPares = [];
+    const divisor =2;
     for (let i = 0; i < a.length; i++) {
-        if (a[i] % 2 === 0) {
-            b.push(a[i]);
+        if (a[i] % divisor === 0) {
+            numerosPares.push(a[i]);
         }
     }
-    return b;
+    return numerosPares;
 }
 
 const x = [1, 2, 3, 4, 5, 6];
